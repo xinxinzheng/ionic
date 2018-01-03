052a4d257e9893e026512872ef4c52c14a816511
unknown <zhengxinxin@zhengxinxin-pc.ESG.360ES.CN>
2018-01-03 18:36:31 +0800

    all

--- myApp/src/pages/tabs/tabs.ts
+++ myApp/src/pages/tabs/tabs.ts
@@ -1,30 +1,19 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { IonicPage, NavController } from 'ionic-angular';
 
-import { Tab1Root } from '../pages';
-import { Tab2Root } from '../pages';
-import { Tab3Root } from '../pages';
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+import { HomePage } from '../home/home';
 
-@IonicPage()
 @Component({
-  selector: 'page-tabs',
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-  tab1Root: any = Tab1Root;
-  tab2Root: any = Tab2Root;
-  tab3Root: any = Tab3Root;
 
-  tab1Title = " ";
-  tab2Title = " ";
-  tab3Title = " ";
+  tab1Root = HomePage;
+  tab2Root = AboutPage;
+  tab3Root = ContactPage;
 
-  constructor(public navCtrl: NavController, public translateService: TranslateService) {
-    translateService.get(['TAB1_TITLE', 'TAB2_TITLE', 'TAB3_TITLE']).subscribe(values => {
-      this.tab1Title = values['TAB1_TITLE'];
-      this.tab2Title = values['TAB2_TITLE'];
-      this.tab3Title = values['TAB3_TITLE'];
-    });
+  constructor() {
+
   }
 }
